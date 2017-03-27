fe3e8298fa729f2a133b432e07df336e1bca3504
sbyoon <sbyoon@mpole.co.kr>
2017-03-27 22:37:18 +0900

    -공부

--- CreateDevice/QuadTreeLod/Dib.h
+++ CreateDevice/QuadTreeLod/Dib.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <Windows.h>
+
+#define ALIGN_2B( a )				((a)%2 ? (a) + 1 : (a))
+#define ALIGN_4B( a )				((a)%4 ? (a) + 4 - ((a)%4) : (a))
+
+#define DIB_HEADER( a )				((LPBITMAPINFOHEADER)(a))
+#define DIB_BMI( a )				((LPBITMAPINFO)(a))
+#define DIB_CX( a )					(DIB_HEADER(a)->biWidth)
+#define DIB_CY( a )					(DIB_HEADER(a)->biHeight)
+#define DIB_BPP( a )				(DIB_HEADER(a)->biBitCount)
+#define DIB_PALSIZE( a )			(DIB_BPP(a) == 24 ? 0 : sizeof(RGBQUAD) * (1 << DIB_BPP(a)))
+#define DIB_LINESIZE ( a )			(ALIGN_4B(DIB_CX(a) * DIB_BPP(a) / 8))
+#define DIB_DATA( a )				((LPBYTE)(a) + sizeof(BITMAPINFOHEADER) + DIB_PALSIZE(a))
+#define DIB_DATASIZE( a )			(DIB_LINESIZE(a) * DIB_CY(a))
+#define DIB_HEADERSIZE ( a )		(sizeof(BITMAPINFOHEADER))
+#define DIB_SIZE ( a )				(DIB_HEADERSIZE(a) + DIB_PALSIZE(a) + DIB_DATASIZE(a))
--- CreateDevice/QuadTreeLod/ZFLog.h
+++ CreateDevice/QuadTreeLod/ZFLog.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <Windows.h>
+#include <stdio.h>
+#include "define.h"
+
+#define ZF_LOG_TARGET_CONSOLE 0x00000001
+#define ZF_LOG_TARGET_FILE    0x00000002
+#define ZF_LOG_TARGET_WINDOW  0x00000004
+#define ZF_LOG_TARGET_ALL (ZF_LOG_TARGET_CONSOLE|ZF_LOG_TARGET_FILE|ZF_LOG_TARGET_WINDOW)
+#define ZF_LOG_WINDOW_TITLE "ZEngine F_Killer"
+#define ZF_LOG_WINDOW_CX 300
+#define ZF_LOG_WINDOW_CY 600
+
+class ZFLog
+{
+public:
+	ZFLog(UINT32 nTarget, LPSTR szFilename = NULL);
+	~ZFLog();
+	void CreateLogWindow();
+	int Log(LPSTR fmt, ...);
+
+private:
+	static LRESULT CALLBACK
+		ZFLog::WndProcZFLog(HWND hwnd, UINT uMsg, WPARAM wPAram, LPARAM lParam);
+
+};
+
+DECLARE  ZFLog* g_pLog;
